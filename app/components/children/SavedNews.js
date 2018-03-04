3353842d68a37d3c142a5425a07367d0ea8b2963
Mohammed Alnasseri <mohammedalnasseri@Mohammeds-MacBook-Pro.local>
2018-03-04 13:26:26 -0500

    added images and byline

--- app/components/children/SavedNews.js
+++ app/components/children/SavedNews.js
@@ -185,10 +185,10 @@ class SavedNews extends Component {
                             {news.headline}
                         </h5></a>
                         <p>{news.snippet}</p>
-                         <small><strong><p>{news.byline}</p></strong></small>
+                         <small><strong><p>{news.byline}</p><cite title="Source Title">{news.date.substring(0, 10)}</cite></strong></small>
                         <div className="text">
                             <small>
-                                <cite title="Source Title">{news.date.substring(0, 10)}</cite>
+                                
                             </small>
                             
                         </div>
