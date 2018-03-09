9bcf3fc79592452400e02431238d93be1b9819cd
Mohammed Alnasseri <mohammedalnasseri@Mohammeds-MacBook-Pro.local>
2018-03-08 23:59:26 -0500

    added images and byline

--- app/components/children/SavedNews.js
+++ app/components/children/SavedNews.js
@@ -150,11 +150,11 @@ class SavedNews extends Component {
                                 />
                                 {this.state.savedNews.map((news, i)=> {
                                 return (
-                                    <tr >
+                                    <tr key={i}>
                                        {this.renderSavedNotes(i)}
                                     </tr>
                                 );
-                            })}
+                            }.bind(this))}
 
                                 
                             </div>
