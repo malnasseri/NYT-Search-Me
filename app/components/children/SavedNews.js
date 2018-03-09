031d0f39138a402de9c2ca867642086a0be54c49
Mohammed Alnasseri <mohammedalnasseri@Mohammeds-MacBook-Pro.local>
2018-03-09 03:33:21 -0500

    added images and byline

--- app/components/children/SavedNews.js
+++ app/components/children/SavedNews.js
@@ -4,6 +4,53 @@ import React, { Component } from 'react';
 // Helper for making AJAX requests to our API
 import helpers from "../utils/helpers";
 
+
+import Modal from 'react-bootstrap-modal';
+
+class ModalExample extends Component {
+
+  render(){
+    let closeModal = () => this.setState({ open: false })
+
+    let saveAndClose = () => {
+      api.saveData()
+        .then(() => this.setState({ open: false }))
+    }
+
+    return (
+      <div>
+        <button type='button'>Launch modal</button>
+
+        <Modal
+          show={this.state.open}
+          onHide={closeModal}
+          aria-labelledby="ModalHeader"
+        >
+          <Modal.Header closeButton>
+            <Modal.Title id='ModalHeader'>A Title Goes here</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>Some Content here</p>
+          </Modal.Body>
+          <Modal.Footer>
+            // If you don't have anything fancy to do you can use
+            // the convenient `Dismiss` component, it will
+            // trigger `onHide` when clicked
+            <Modal.Dismiss className='btn btn-default'>Cancel</Modal.Dismiss>
+
+            // Or you can create your own dismiss buttons
+            <button className='btn btn-primary' onClick={saveAndClose}>
+              Save
+            </button>
+          </Modal.Footer>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+
+
 class SavedNews extends Component {
     constructor(){
         super();
@@ -129,10 +176,10 @@ class SavedNews extends Component {
     }
 
     renderNotesModal(){
-         this.state.savedNews.map((i)=>{
+         return this.state.savedNews.map((news, i)=>{
         return(
                 
-                <div className="modal">
+                <div className="modal" key={news._id}>
 
                     <div className="modal-dialog">
                         <div className="modal-content">
