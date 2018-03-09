bee6d040e84e0ab77a4b9135ada756e542fc4db5
Mohammed Alnasseri <mohammedalnasseri@Mohammeds-MacBook-Pro.local>
2018-03-08 20:20:49 -0500

    added images and byline

--- app/components/children/SavedNews.js
+++ app/components/children/SavedNews.js
@@ -102,44 +102,7 @@ class SavedNews extends Component {
         this.setState({ noteInput: event.target.value});
     }
 
-    renderNotesModal(){
-        return(
-                <div className="modal" key={news._id}>
-                    <div className="modal-dialog">
-                        <div className="modal-content">
-                        <div className="modal-header">
-                            <h4 className="modal-title">Please add your note here.</h4>
-                        </div>
-                        <div className="modal-body">
-                            <div className="form-group">
-                                <label className="control-label">Type here.</label>
-                                <input 
-                                value={this.state.noteInput}
-                                className="form-control input-lg" 
-                                id="focusedInput" 
-                                type="text"
-                                onChange={this.handleNoteInputChange} 
-                                />
-                            </div>
-                        </div>
-                        {this.renderSavedNotes(i)}
-                        <div className="modal-footer">
 
-                            <button type="button" 
-                            className="btn btn-default" 
-                            data-dismiss="modal"
-                            onClick={()=>this.setState({newsID: ""})}
-                            >Close</button>
-                            <button type="button" 
-                            className="btn btn-primary save-btn"
-                            onClick={this.handleSaveNote}
-                            >Save</button>
-                        </div>
-                        </div>
-                    </div>
-                </div>
-        )
-    }
 
     renderSavedNotes(i){
         return this.state.savedNews[i].note.map((note, index)=>{
@@ -306,6 +269,44 @@ class SavedNews extends Component {
                   </div>
                 )
                     }
+                     renderNotesModal(){
+        return(
+                <div className="modal" key={news._id}>
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                        <div className="modal-header">
+                            <h4 className="modal-title">Please add your note here.</h4>
+                        </div>
+                        <div className="modal-body">
+                            <div className="form-group">
+                                <label className="control-label">Type here.</label>
+                                <input 
+                                value={this.state.noteInput}
+                                className="form-control input-lg" 
+                                id="focusedInput" 
+                                type="text"
+                                onChange={this.handleNoteInputChange} 
+                                />
+                            </div>
+                        </div>
+                        {this.renderSavedNotes(i)}
+                        <div className="modal-footer">
+
+                            <button type="button" 
+                            className="btn btn-default" 
+                            data-dismiss="modal"
+                            onClick={()=>this.setState({newsID: ""})}
+                            >Close</button>
+                            <button type="button" 
+                            className="btn btn-primary save-btn"
+                            onClick={this.handleSaveNote}
+                            >Save</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+        )
+    }
             })
     }
     render() {
