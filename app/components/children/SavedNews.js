77a3097d57b769beb3df56792cc4f5b5d4f87a11
Mohammed Alnasseri <mohammedalnasseri@Mohammeds-MacBook-Pro.local>
2018-03-09 01:07:16 -0500

    added images and byline

--- app/components/children/SavedNews.js
+++ app/components/children/SavedNews.js
@@ -68,7 +68,7 @@ class SavedNews extends Component {
         
         console.log("+++++++++++++++++++++++++++++++" + id);
         // toggle modal to input notes
-        $('.modal').modal();
+        $('.modal').modal(id);
 
 
         // set newsId state to whateever id in the add notes buttons clicked
@@ -129,7 +129,7 @@ class SavedNews extends Component {
     }
 
     renderNotesModal(){
-          this.state.savedNews.map((news, i)=>{
+         return this.state.savedNews.map((news, i)=>{
         return(
                 
                 <div className="modal" key={news._id}>
