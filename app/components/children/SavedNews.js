375ac660ebf1b854eb84d4036b27602c914ebd65
Mohammed Alnasseri <mohammedalnasseri@Mohammeds-MacBook-Pro.local>
2018-03-09 03:07:44 -0500

    added images and byline

--- app/components/children/SavedNews.js
+++ app/components/children/SavedNews.js
@@ -155,7 +155,7 @@ class SavedNews extends Component {
                                 {this.renderSavedNotes(i)}
                                     </div>
 
-                                ) })  
+                                ) })}  
 
 
 
